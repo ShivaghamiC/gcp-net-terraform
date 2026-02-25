ab241055cb75ad12d47b061be09310e886eb0f95
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 04:49:30 +0530

    Delete gcp-net-terraform_modules_lb_external_https_variables.tf

--- gcp-net-terraform_modules_lb_external_https_variables.tf
+++
@@ -1,7 +0,0 @@
-
-variable "name"         { type = string }
-variable "region"       { type = string }
-variable "network"      { type = string }
-variable "subnet"       { type = string }
-variable "domain"       { type = string }
-variable "machine_type" { type = string default = "e2-micro" }
