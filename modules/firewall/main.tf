e057a605e0091bb0456c3f0ae31ce1764eff0fba
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 05:16:15 +0530

    Add files via upload

---
+++ modules/firewall/main.tf
@@ -0,0 +1,22 @@
+
+locals { iap_range = "35.235.240.0/20" }
+
+resource "google_compute_firewall" "allow_iap_ssh" {
+  name      = "${var.network_name}-allow-iap-ssh"
+  network   = var.network_name
+  direction = "INGRESS"
+  priority  = 1000
+  source_ranges = [local.iap_range]
+  allow { protocol = "tcp" ports = ["22"] }
+  target_service_accounts = var.ssh_target_sas
+}
+
+resource "google_compute_firewall" "allow_web" {
+  name      = "${var.network_name}-allow-web"
+  network   = var.network_name
+  direction = "INGRESS"
+  priority  = 1000
+  source_ranges = var.web_source_ranges
+  allow { protocol = "tcp" ports = ["80","443"] }
+  target_tags = var.web_target_tags
+}
---
+++ modules/firewall/variables.tf
@@ -0,0 +1,5 @@
+
+variable "network_name"      { type = string }
+variable "ssh_target_sas"    { type = list(string) default = [] }
+variable "web_source_ranges" { type = list(string) default = ["0.0.0.0/0"] }
+variable "web_target_tags"   { type = list(string) default = ["web"] }
