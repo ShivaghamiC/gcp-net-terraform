694ab7c26827f8e763cca1b4fe2c7727d4f1286a
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 04:50:31 +0530

    Delete gcp-net-terraform_modules_nat_variables.tf

--- gcp-net-terraform_modules_nat_variables.tf
+++
@@ -1,5 +0,0 @@
-
-variable "name"              { type = string }
-variable "region"            { type = string }
-variable "network"           { type = string }
-variable "subnet_self_links" { type = list(string) }
