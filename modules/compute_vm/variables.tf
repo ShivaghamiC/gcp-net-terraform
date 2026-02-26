f68d5a31d14b519ad6be080a97c799294865acc8
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 07:20:31 +0530

    Update variables.tf

--- modules/compute_vm/variables.tf
+++ modules/compute_vm/variables.tf
@@ -1,9 +1,39 @@
+variable "name" {
+  type        = string
+  description = "The name of the VM instance." [cite: 1]
+}
 
-variable "name"          { type = string }
-variable "sa_id"         { type = string }
-variable "zone"          { type = string }
-variable "machine_type"  { type = string default = "e2-micro" }
-variable "network"       { type = string }
-variable "subnet"        { type = string }
-variable "tags"          { type = list(string) default = [] }
-variable "startup_script"{ type = string default = "" }
+variable "sa_id" {
+  type        = string
+  description = "The account ID for the service account." [cite: 1]
+}
+
+variable "zone" {
+  type        = string
+  default     = "us-central1-a" [cite: 1]
+}
+
+variable "machine_type" {
+  type        = string
+  default     = "e2-micro" [cite: 1]
+}
+
+variable "network" {
+  type        = string
+  description = "The VPC network name." [cite: 1]
+}
+
+variable "subnet" {
+  type        = string
+  description = "The subnetwork name." [cite: 1]
+}
+
+variable "tags" {
+  type        = list(string)
+  default     = [] [cite: 1]
+}
+
+variable "startup_script" {
+  type        = string
+  default     = "" 
+}
