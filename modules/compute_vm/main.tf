532409a8bf151d3065cb4d6355991fd32bf53c2c
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 07:22:00 +0530

    Refactor Google Compute VM resource configuration

--- modules/compute_vm/main.tf
+++ modules/compute_vm/main.tf
@@ -1,29 +1,40 @@
-
+# Create the Service Account
 resource "google_service_account" "sa" {
   account_id   = var.sa_id
-  display_name = "VM SA for ${var.name}"
+  display_name = "VM SA for ${var.name}" 
 }
 
+# Create the Private Compute Instance
 resource "google_compute_instance" "vm" {
   name         = var.name
   zone         = var.zone
   machine_type = var.machine_type
-  tags         = var.tags
-  boot_disk { 
-  initialize_params { 
-    image = "debian-cloud/debian-12" 
+  tags         = var.tags 
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12" 
+    }
   }
-  }
+
   network_interface {
-    #network    = var.network
+    network    = var.network
     subnetwork = var.subnet
-    # No external IP => private-only instance
+    # By omitting access_config, the VM gets no external IP 
   }
+
   service_account {
     email  = google_service_account.sa.email
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"] [cite: 4]
   }
-  metadata = { enable-oslogin = "TRUE" }
-  shielded_instance_config { enable_secure_boot = true }
-  metadata_startup_script = var.startup_script
+
+  metadata = {
+    enable-oslogin = "TRUE" [cite: 4]
+  }
+
+  shielded_instance_config {
+    enable_secure_boot = true [cite: 4]
+  }
+
+  metadata_startup_script = var.startup_script [cite: 4]
 }
