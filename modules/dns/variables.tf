76090dc7b37357e7397cf3492e8911aebc0746ca
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 19:01:32 +0530

    Format private_records variable definition

--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -6,4 +6,7 @@ variable "private_zone_fqdn"  { type = string }
 variable "app_name"           { type = string }
 variable "external_ip"        { type = string }
 variable "network_self_link"  { type = string }
-variable "private_records" { type = map(string) default = {} }
+variable "private_records" { 
+  type = map(string) 
+  default = {} 
+}
