2b0d9e8039919e84238accc6aed666d0c292abb5
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 08:24:24 +0530

    Enhance variable definitions in versions.tf
    Added descriptions and default value for variables.

--- versions.tf
+++ versions.tf
@@ -1,24 +1,40 @@
-
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    google = { source = "hashicorp/google", version = "~> 5.40" }
-    google-beta = { source = "hashicorp/google-beta", version = "~> 5.40" }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.40"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.40"
+    }
   }
 }
 
 provider "google" {
-  project                    = var.project_id
-  region                     = var.region
+  project                     = var.project_id
+  region                      = var.region
   impersonate_service_account = var.deploy_sa_email
 }
 
 provider "google-beta" {
-  project                    = var.project_id
-  region                     = var.region
+  project                     = var.project_id
+  region                      = var.region
   impersonate_service_account = var.deploy_sa_email
 }
 
-variable "project_id" { type = string }
-variable "region"     { type = string }
-variable "deploy_sa_email" { type = string }
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  default     = "asia-south1"
+}
+
+variable "deploy_sa_email" {
+  type        = string
+  description = "The email of the service account to impersonate"
+}
