fec7add666d22eecc3fd13b1ad2b070b10b98a69
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 20:29:11 +0530

    Add files via upload

---
+++ versions.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.40"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.40"
+    }
+  }
+}
+
+provider "google" {
+  project                     = var.project_id
+  region                      = var.region
+  impersonate_service_account = var.deploy_sa_email
+}
+
+provider "google-beta" {
+  project                     = var.project_id
+  region                      = var.region
+  impersonate_service_account = var.deploy_sa_email
+}
+
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+  default     = "terraform-488518"
+}
+
+variable "region" {
+  type        = string
+  default     = "asia-south1"
+}
+
+variable "deploy_sa_email" {
+  type        = string
+  description = "The email of the service account to impersonate"
+  default     = "terraform-deployer@terraform-488518.iam.gserviceaccount.com"
+}
