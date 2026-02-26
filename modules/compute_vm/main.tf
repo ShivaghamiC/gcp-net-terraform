3d6cd5cdab721418782f904b8a55b5697e9cd73b
ShivaghamiC <csshiva3@gmail.com>
2026-02-26 05:57:22 +0530

    Update boot disk configuration for VM instance

--- modules/compute_vm/main.tf
+++ modules/compute_vm/main.tf
@@ -9,7 +9,14 @@ resource "google_compute_instance" "vm" {
   zone         = var.zone
   machine_type = var.machine_type
   tags         = var.tags
-  boot_disk { initialize_params { image = "debian-cloud/debian-12" } }
+  boot_disk 
+  { 
+    initialize_params 
+    { 
+      image_family = "debian-cloud" 
+      image_project = "debian-12"
+    }
+  }
   network_interface {
     network    = var.network
     subnetwork = var.subnet
